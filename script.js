b51eea14644d0a55fb653e82482c13a64776809b
bou1der <krowkin@bk.ru>
2023-11-13 20:45:50 +0300

    debug carousel

--- script.js
+++ script.js
@@ -1,60 +1,26 @@
-// document.getElementById("carousel")
-// // var carouselblock = new Array();
-// var carouselblock = document.querySelector(".carousel")
-// // console.log(carouselblock)
-
-// // for(let a = 0; a < carouselblock.length; a++)
-// // {
-// //     console.log(a)
-// //     carouselblock.length[a].remove(carouselblock);
-
-// // }
-// let i = 0;
-// function next()
-// {
-    
-    
-//     if(true)
-//     {
-//         i++
-//         let firstEl = document.querySelector(".carousel > div")
-
-//         var test = firstEl.cloneNode(true);
-//         carouselblock.append(test);
-//         carouselblock.append(test);
-//         console.log(carouselblock)
-//         let move = (344 * -i);
-//         carousel.style.transform = "translateX("+ move + "px)";
-//     }
-    
-// }
-// function prev()
-// {
-    
-//     if(true)
-//     {
-//         i--
-//         let move = (-344 * i);
-//         carousel.style.transform = "translateX("+ move + "px)";
-//     }
-// }
 const carousel = document.querySelector('.carousel')
 var direction;
 
-document.querySelector('.next').addEventListener('click', function(){
+var nextButton = document.querySelector('.next')
+nextButton.onclick = next;
+function next(){
     direction = -1;
     carousel.style.justifyContent = 'flex-start';
     carousel.style.transform = "translateX("+ (-344) + "px)";
-});
+    nextButton.onclick = null;
+};
 
-document.querySelector('.prev').addEventListener('click', function(){
+var prevButton = document.querySelector('.prev')
+prevButton.onclick = prev ;
+function prev(){
     direction = 1;
     carousel.style.justifyContent = 'flex-end';
     carousel.style.transform = "translateX("+ 344 + "px)";
-});
+    prevButton.onclick = null;
+
+}
 
 carousel.addEventListener('transitionend', function(){
-
     if(direction == -1){
         carousel.appendChild(carousel.firstElementChild);
     }else if(direction == 1){
@@ -65,6 +31,9 @@ carousel.addEventListener('transitionend', function(){
     carousel.style.transform = 'translateX(0)';
     setTimeout(function(){
         carousel.style.transition = 'transform 0.1s ease-in-out';
-    },500)
-    
+    },100)
+    setTimeout(function(){
+        prevButton.onclick = prev;
+        nextButton.onclick = next;
+    }, 150)
 });
